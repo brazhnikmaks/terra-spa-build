c21ccacb1245ca64ea6c854fe8c9c5e236689d59
Maksim Brazhnik <m.brazhnik@tekrum.pr>
2019-09-06 23:13:04 +0300

    work with apolon

---
+++ precache-manifest.20e471e34bb21791cd42f7d6859a3eca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee98a67398e14e20f4c22d001844f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c2d4a47a7db031c991a",
+    "url": "/static/css/main.327947e1.chunk.css"
+  },
+  {
+    "revision": "4d97b52afb0218dc550a",
+    "url": "/static/js/2.6505022e.chunk.js"
+  },
+  {
+    "revision": "9c2d4a47a7db031c991a",
+    "url": "/static/js/main.cc6f8e73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
